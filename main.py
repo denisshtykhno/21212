962fa1a742768275b983306f7d4414499dd6f540
denis <denis.shtykhno@gmail.com>
2023-11-18 13:03:07 +0200

    add file

--- main.py
+++ main.py
@@ -1,71 +1,22 @@
-import sqlite3
-import requests
-from bs4 import BeautifulSoup
+# connection = sqlite3.connect('shop.db')
+# cursor = connection.cursor()
+# cursor.execute("""CREATE TABLE IF NOT EXISTS film (id INTEGER PRIMARY KEY,
+#                                      title VARCHAR(100) UNIQUE NOT NULL,
+#                                      genre VARCHAR(100) NOT NULL,
+#                                      year DATE NOT NULL,
+#                                      director VARCHAR(100));""")
+# cursor.execute("""CREATE TABLE IF NOT EXISTS directors (id INTEGER PRIMARY KEY,
+#                                      name VARCHAR(100) UNIQUE NOT NULL,
+#                                      birth DATE NOT NULL);""")
+# cursor.execute("""INSERT INTO film (title, genre, year, director) VALUES ('Titanic', 'drama', '1998-11-23', 'Bob Smith'),
+#                                                                          ('Romeo and Juliette', 'drama', '1999-06-24', 'Jack Jackson')""")
+#
+# connection.commit()
+# connection.close()
 
-def create_database():
-
-    conn = sqlite3.connect('movies_weather_database.db')
-    cursor = conn.cursor()
-
-
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS movies (
-            id INTEGER PRIMARY KEY,
-            title TEXT UNIQUE,
-            genre TEXT,
-            release_year INTEGER,
-            director_id INTEGER,
-            FOREIGN KEY (director_id) REFERENCES directors(id)
-        )
-    ''')
-
-
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS directors (
-            id INTEGER PRIMARY KEY,
-            first_name TEXT,
-            last_name TEXT
-        )
-    ''')
-
-    cursor.executemany('''
-        INSERT INTO directors (first_name, last_name) VALUES (?, ?)
-    ''', [
-        ('Steven', 'Spielberg'),
-        ('Christopher', 'Nolan'),
-        ('Quentin', 'Tarantino'),
-        ('Martin', 'Scorsese'),
-        ('Hayao', 'Miyazaki')
-    ])
-
-
-    cursor.executemany('''
-        INSERT INTO movies (title, genre, release_year, director_id) VALUES (?, ?, ?, ?)
-    ''', [
-        ('Jurassic Park', 'Adventure', 1993, 1),
-        ('Inception', 'Sci-Fi', 2010, 2),
-        ('Pulp Fiction', 'Crime', 1994, 3),
-        ('The Irishman', 'Crime', 2019, 4),
-        ('Spirited Away', 'Animation', 2001, 5)
-    ])
-
-
-    conn.commit()
-    conn.close()
-
-def get_weather():
-
-    url = "https://weather.com/weather/tenday/l/your_location_code"  # Замініть "your_location_code" на код вашого місця
-
-
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'lxml')
-
-
-    forecast_container = soup.find('div', class_='DailyForecast--DisclosureList')
-
-    if forecast_container:
-
-        forecast_items = forecast_container.find_all('div', class_='DailyContent--dailyContent--3obym')
-
-
+# connection = sqlite3.connect('shop.db')
+# cursor = connection.cursor()
+# cursor.execute("""SELECT * FROM film WHERE id = 1; """)
+# data = cursor.fetchall()
+# print(data)
+# connection.close()
